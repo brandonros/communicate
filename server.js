2680458178fb39f1836135b92c412d4e2c7721ff
Brandon Ros <brandonros1@gmail.com>
2015-12-05 13:57:09 -0500

    not sure if i want express or not yet

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var diffpatcher = require('jsondiffpatch').create();
 var cache = require('memory-cache');
 var uuid = require('uuid');
 
-function notify_channel(channel, old_version, new_version) {
-	io.to(channel).emit('patch', { old_version: old_version, new_version: new_version });
+function notify_channel(channel, name, old_version, new_version, patch) {
+	io.to(channel).emit('patch', { name: name, old_version: old_version, new_version: new_version, patch: patch });
 }
 
 function calculate_patch(old_store, new_store) {
@@ -22,8 +22,8 @@ function get_patch(old_version, new_version) {
 	return cache.get('patch:' + old_version + ':' + new_version);
 }
 
-function get_patches(body) {
-	return Promise.all(body['stores'].map(function (store) {
+function get_patches(stores) {
+	return Promise.all(stores.map(function (store) {
 		return get_patch(store['old_version'], store['new_version']);
 	}));
 }
@@ -32,12 +32,26 @@ function get_store(version) {
 	return cache.get('store:' + version);
 }
 
-function set_store(version, docs) {
-	return cache.set('store:' + version, docs);
+function set_store(version, store) {
+	return cache.set('store:' + version, store);
+}
+
+function get_version(name) {
+	return cache.get('version:' + name);
+}
+
+function set_version(name, version) {
+	return cache.set('version:' + name, version);
+}
+
+function get_versions(names) {
+	return Promise.all(names.map(function (name) {
+		return get_version(name);
+	}));
 }
 
 function update_store(channel, name, new_store) {
-	return cache.get('version:' + name)
+	return get_version(name)
 		.then(function (old_version) {
 			return get_store(old_version)
 				.then(function (old_store) {
@@ -46,9 +60,28 @@ function update_store(channel, name, new_store) {
 
 					return store_patch(old_store, old_store, new_store, new_version)
 						.then(function () {
-							send_to_channel(channel, old_version, new_version);
+							return set_version(name, new_version);
+								.then(function () {
+									send_to_channel(channel, name, old_version, new_version, patch);
+								});
 						});
 				});
+		});
+}
+
+function init_stores(name) {
+	var store = {
+		account: { id: 1, name: 'lol' },
+		items: [{ id: 1, name: 'extra lol' }],
+		balance: 300,
+		customers: [{ id: 1, name: 'ronnie' }]
+	};
+
+	var version = uuid.v4();
+
+	return set_store(verion, store)
+		.then(function () {
+			return set_version(name, version);
 		});
 }
 
@@ -59,19 +92,17 @@ function init_server(port) {
 		});
 	});
 
-	app.post('/get_patches', function (req, res) {
-		var body = req['body'];
-
-		get_patches(req['body'])
-		.then(function (patches) {
-			res.send(patches);
-		})
-		.catch(function (err) {
-			res.status(500).send({ error: err['stack'] });
-		});
-	});
-
 	app.listen(port);
 }
 
-init_server(9090);
+init_stores('1')
+.then(function () {
+	init_server(9090);	
+
+	console.log('Listening...');
+})
+.catch(function (err) {
+	console.error(err['stack']);
+
+	process.exit(1);
+});
